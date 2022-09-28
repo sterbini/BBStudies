fb883c28b29df2c65fbfaa244457a64602f7c844
pbelange <philippe.belanger@cern.ch>
2022-09-28 12:44:33 +0200

    AddedXsuitePlus, with inspect and knobs

---
+++ BBStudies/Base/XsuitePlus.py
@@ -0,0 +1,60 @@
+
+#============================
+from xdeps.refs import ARef
+import rich
+import re
+import numpy as np
+
+
+class RenderingKnobs(object):   
+    def __init__(self, my_dict):
+        for key in my_dict.keys():
+            setattr(self, key, my_dict[key])
+
+
+def knobs(self,render = True):
+    _fields = self._value._fields
+    
+    sub_knobs   = []
+    print_names = {}
+    for key in _fields:
+
+        _attr = getattr(self,key)
+
+        # List or not list
+        if isinstance(_attr._value, (type(np.array([])), list)):
+            _expr = [_attr[i]._expr for i in range(len(_attr._value))]
+        else:
+            _expr = _attr._expr
+
+        if _expr is None:
+            print_names[key] = None
+        else:
+            print_names[key] = str(_expr)
+
+        if str(_expr)[0] + str(_expr)[-1] == '[]':
+            matches    = re.findall(r"[^[]*\[([^]]*)\]", str(_expr)[1:-1])
+        else:
+            matches    = re.findall(r"[^[]*\[([^]]*)\]", str(_expr))
+        sub_knobs += [m[1:-1] for m in matches]
+
+    print_values = {}
+    for _var in list(set(sub_knobs)):
+        _value = self._manager.containers['vars'][_var]._value
+        print_values[f"'{_var}'"] = _value
+
+    printable = {**print_values,**{ 30*'-': 30*'-'},**print_names}
+    
+    # Either shows the knobs or return list of knobs
+    if render:
+        rich.inspect(RenderingKnobs(printable),title=str(self._value), docs=False)
+    else:
+        return list(set(sub_knobs))
+
+
+def inspect(self,**kwargs):
+    return rich.inspect(self._value,**kwargs)
+
+ARef.inspect = inspect
+ARef.knobs = knobs
+#============================
