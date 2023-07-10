c61316f87a6efd3177e5694a6d3c5f75f0ca80d7
Guido Sterbini <guido.sterbini@cern.ch>
2023-07-10 22:55:08 +0200

    I forgot a ../

--- BBStudies/Run3_configuration/run.sh
+++ BBStudies/Run3_configuration/run.sh
@@ -1,5 +1,5 @@
 source ../../Executables/py_BB/bin/activate
 cd 1_build_distr_and_collider
 python 1_build_distr_and_collider.py
-cd 2_configure_and_track/
+cd ../2_configure_and_track/
 python 2_configure_and_track.py 
